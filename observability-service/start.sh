1e6fa30d10ddb05f38758f5bec672c55fbb11317
Anil Jadhav <aniljadhav@gmail.com>
2026-06-16 16:10:04 +0530

    fix: Grafana crash - substitute PAGERDUTY_INTEGRATION_KEY via sed in start.sh, revert memory to 512MB

--- observability-service/start.sh
+++ observability-service/start.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# Substitute Prometheus targets based on environment
-# In docker-compose: PRODUCT_TARGET=product-service:4001, ORDER_TARGET=order-service:4002
-# In ECS: set via env vars to use ALB internal or service connect
 PRODUCT_TARGET=${PRODUCT_TARGET:-product-service:4001}
 ORDER_TARGET=${ORDER_TARGET:-order-service:4002}
+PAGERDUTY_KEY=${PAGERDUTY_INTEGRATION_KEY:-dummy-key-for-local}
 
 sed -i "s|PRODUCT_TARGET|${PRODUCT_TARGET}|g" /etc/prometheus/prometheus.yml
 sed -i "s|ORDER_TARGET|${ORDER_TARGET}|g" /etc/prometheus/prometheus.yml
+sed -i "s|\${PAGERDUTY_INTEGRATION_KEY}|${PAGERDUTY_KEY}|g" /etc/grafana/provisioning/alerting/contactpoints.yml
 
 # Start Prometheus in background
 prometheus --config.file=/etc/prometheus/prometheus.yml \
